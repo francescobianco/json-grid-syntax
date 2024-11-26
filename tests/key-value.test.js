c73fb4b32c1a300af0b823fe06bfbc5d742a8b09
francesco <info.francescobianco@gmail.com>
2024-11-26 11:28:03 +0100

    First commit WIP

--- tests/key-value.test.js
+++ tests/key-value.test.js
@@ -36,3 +36,40 @@ test('key value expand on branch', () => {
         ["surname", "Claus"]
     ]);
 });
+
+test('key value expand with mixed layout', () => {
+    const inputJson = {
+        people: [
+            {
+                parents: [
+                    {
+                        n: "A",
+                        e: "C",
+                    }
+                ]
+            }
+        ],
+        invoice: {
+            t: "B"
+        }
+    };
+
+    const outputGrid = extractAsGrid(
+        inputJson,
+        "people.0.parents.0 > #, invoice > #; " +
+        "invoice > #, people.0.parents.0 > #; " +
+        "people.0.parents.0 > #, invoice > #; " +
+        "invoice > #, people.0.parents.0 > #"
+    )
+
+    expect(outputGrid).toStrictEqual([
+        [ 'n', 'A', 't', 'B' ],
+        [ 'e', 'C' ],
+        [ 't', 'B', 'n', 'A' ],
+        [ undefined, undefined, 'e', 'C' ],
+        [ 'n', 'A', 't', 'B' ],
+        [ 'e', 'C' ],
+        [ 't', 'B', 'n', 'A' ],
+        [ undefined, undefined, 'e', 'C' ]
+    ]);
+});
